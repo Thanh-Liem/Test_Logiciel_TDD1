0c2cd19195b129087b76e42dc3fe933b05275a53
Thanh-Liem <ta.thanh-liem.sup@hotmail.com>
2022-12-29 16:28:00 +0100

    Implementation des fonctions d'initialisation de la table dans la base de donnee et la verification

--- ex2.py
+++ ex2.py
@@ -3,8 +3,50 @@ import string
 
 def reset_init_db(db_name):
 
-	return -1
+	if not isinstance(db_name, str):
+		return False
+
+	if db_name == "":
+		return False
+
+	# ouverture/initialisation de la base de donnee
+	conn = sqlite3.connect(db_name)
+	conn.row_factory = sqlite3.Row
+	c = conn.cursor()
+
+	# Action
+	c.execute("drop table if exists Utilisateur")
+	c.execute("create table if not exists Utilisateur(username TEXT NOT NULL PRIMARY KEY, password TEXT NOT NULL, links TEXT)")
+
+	c.fetchall()
+	
+	# fermeture de la base de donnee
+	conn.commit()
+	conn.close()
+
+	return True
 
 def verif_table(db_name):
 
-	return -1
+	if not isinstance(db_name, str):
+		return False
+
+	if db_name == "":
+		return False
+
+	# ouverture/initialisation de la base de donnee
+	conn = sqlite3.connect(db_name)
+	conn.row_factory = sqlite3.Row
+	c = conn.cursor()
+
+	c.execute("select exists (select name from sqlite_schema where type='table' and name='Utilisateur')")
+
+	ret = c.fetchone()[0]
+
+	# fermeture de la base de donnee
+	conn.commit()
+	conn.close()
+
+	if ret == 0:
+		return False
+	return True
